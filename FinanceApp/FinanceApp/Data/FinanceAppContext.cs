c6dd42f89511003b8819a8ea52f197f475abbe16
tamanazehra <zehratamana3@gmail.com>
2025-05-27 16:16:16 +0530

    finance-app

---
+++ FinanceApp/FinanceApp/Data/FinanceAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Models.Expense> Expenses { get; set; } 
+
+    }
+}
