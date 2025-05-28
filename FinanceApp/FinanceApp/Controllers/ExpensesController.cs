ffd7d4dd786ae4b46a9fa10dbb48b06110916414
tamanazehra <zehratamana3@gmail.com>
2025-05-28 11:42:14 +0530

    Created Mehtods

---
+++ FinanceApp/FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,38 @@
+﻿using FinanceApp.Data;
+using FinanceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly FinanceAppContext _context;
+        public ExpensesController(FinanceAppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+            return View(expenses);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid) 
+            { 
+                _context.Expenses.Add(expense);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+    }
+}
